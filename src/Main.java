3d822aede734881fdabdb445f1a34dba97b95fda
Anna Filipova <faunaf@gmail.com>
2020-03-28 22:52:11 +0300

    Add files

---
+++ src/Main.java
@@ -0,0 +1,8 @@
+public class Main {
+    public static void main(String[] args) {
+        int ticket = 9_800;
+        int accruals = 20;
+        int mili = ticket / accruals;
+        System.out.println(mili);
+    }
+}
